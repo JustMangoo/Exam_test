d664aabeb9e4b9502b8733486fe9a1e4f7a36629
Aleksis Daugats <daugatsa@gmail.com>
2022-06-13 05:38:07 +0300

    atildes parbaude

--- src/theTest.java
+++ src/theTest.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
 
 public class theTest {
+	
+	static int parbaude(int rez, String atb, String par){
+		if(atb.equals(par)){
+			System.out.println("✔️");
+			rez++;
+		}else{
+			System.out.println("❌");
+		}
+		return rez;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int punkti=0;
+		String atbilde, pareizi;
+		
+		System.out.println("\t-Tests par Do... While-\n(Izvēlies vienu pareizu atbildi )");
+		
+		//Jautājums 1
+		System.out.println("\n\n 1. Kāda ir galvenā atšķirība starp While un Do.. While?"
+						+"\nA) Garāks pieraksts"
+						+"\nB) Nosacījumu pārbauda beigās"
+						+"\nC) Mazāks izpildes laiks"
+						+"\nD) Neviens");
+		pareizi = "B";
+		System.out.print("Atbilde: "); atbilde = scan.next().toUpperCase();
+		punkti = parbaude(punkti, atbilde, pareizi);
+		
+		scan.close();
 
 	}
 
